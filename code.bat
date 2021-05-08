9cb10eb5d88ba1281ac6c647f65aa3626e386469
Seho Eom <seom999@gmail.com>
2021-05-07 19:57:33 -0700

    add curl description

--- code.bat
+++ code.bat
@@ -1,2 +1,7 @@
 echo "starting program"
 echo "text" > text1.txt
+dir > text3.txt
+dir > coomit2.bat
+dir > matrix.py
+ipconfig > result.txt
+ping > result1.txt
--- coomit2.bat
+++ coomit2.bat
@@ -7,15 +7,15 @@
 05/06/2021  08:11 PM    <DIR>          ..
 05/03/2021  07:59 PM                 0 .txt
 05/06/2021  08:12 PM               146 code.bat
-05/06/2021  08:12 PM               908 coomit2.bat
+05/06/2021  08:12 PM                 0 coomit2.bat
 05/03/2021  08:11 PM                51 gitPush.bat
 05/06/2021  07:46 PM                18 matrix - Copy.cs
-05/06/2021  08:12 PM                 0 matrix.py
+05/06/2021  08:11 PM               857 matrix.py
 05/06/2021  08:11 PM               936 result.txt
 05/06/2021  07:38 PM                27 test1.bat
 05/06/2021  08:12 PM                 9 text1.txt
 05/06/2021  08:09 PM             2,804 text2.txt
 05/06/2021  08:12 PM               908 text3.txt
 05/03/2021  07:56 PM    <DIR>          txt
-              11 File(s)          5,807 bytes
+              11 File(s)          5,756 bytes
                3 Dir(s)  336,307,335,168 bytes free
