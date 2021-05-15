177c66e0f3b3be5b777578a51feeb36c75d7835f
unknown <seom999@gmail.com>
2021-05-14 23:10:07 -0400

    adding eventlog & clear content

--- powershell/matrix.py
+++ powershell/matrix.py
@@ -1,21 +1,28 @@
- Volume in drive C is OS
- Volume Serial Number is 36EB-181C
+ Volume in drive C has no label.
+ Volume Serial Number is 9C45-8F94
 
- Directory of C:\Users\seom9\Desktop\txtfile_powershell
+ Directory of C:\Users\Dell13\Desktop\txtfile_powershell\powershell
 
-05/06/2021  08:11 PM    <DIR>          .
-05/06/2021  08:11 PM    <DIR>          ..
-05/03/2021  07:59 PM                 0 .txt
-05/06/2021  08:12 PM               146 code.bat
-05/06/2021  08:12 PM               908 coomit2.bat
-05/03/2021  08:11 PM                51 gitPush.bat
-05/06/2021  07:46 PM                18 matrix - Copy.cs
-05/06/2021  08:12 PM                 0 matrix.py
-05/06/2021  08:11 PM               936 result.txt
-05/06/2021  07:38 PM                27 test1.bat
-05/06/2021  08:12 PM                 9 text1.txt
-05/06/2021  08:09 PM             2,804 text2.txt
-05/06/2021  08:12 PM               908 text3.txt
-05/03/2021  07:56 PM    <DIR>          txt
-              11 File(s)          5,807 bytes
-               3 Dir(s)  336,307,335,168 bytes free
+05/13/2021  11:00 PM    <DIR>          .
+05/13/2021  11:00 PM    <DIR>          ..
+05/13/2021  10:56 PM                 0 .txt
+05/13/2021  10:56 PM               146 code.bat
+05/12/2021  10:56 PM               116 code1.bat
+05/14/2021  10:46 PM             1,288 coomit2.bat
+05/13/2021  10:56 PM                74 curl.txt
+05/13/2021  10:56 PM                 0 forloop.txt
+05/13/2021  10:56 PM                51 gitPush.bat
+05/13/2021  10:56 PM                18 matrix - Copy.cs
+05/14/2021  10:46 PM                 0 matrix.py
+05/13/2021  10:56 PM               936 result.txt
+05/13/2021  10:56 PM             1,697 result1.txt
+05/12/2021  10:57 PM                13 sample.cs
+05/12/2021  10:57 PM                17 sample01.py
+05/13/2021  11:00 PM                64 test02.txt
+05/13/2021  10:56 PM                27 test1.bat
+05/14/2021  10:46 PM                 9 text1.txt
+05/13/2021  10:56 PM             2,804 text2.txt
+05/14/2021  10:46 PM             1,288 text3.txt
+05/13/2021  10:56 PM            27,242 txt.txt
+              19 File(s)         35,790 bytes
+               2 Dir(s)  783,934,803,968 bytes free
